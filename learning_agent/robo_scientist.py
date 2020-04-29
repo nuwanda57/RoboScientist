8cbe5194d25145d9920669a26d2e6bf578332a07
Julia <nuwanda57@yandex.ru>
2020-04-29 12:34:35 +0300

    fix logging error

--- learning_agent/robo_scientist.py
+++ learning_agent/robo_scientist.py
@@ -15,6 +15,21 @@ EpochHistory = namedtuple('EpochHistory', ['mse', 'formula'])
 ExplorationKey = namedtuple('ExplorationKey', ['env', 'theory'])
 
 
+def _create_logger(name):
+    logger = logging.getLogger(name)
+    logger.setLevel(logging.DEBUG)
+    fh = logging.FileHandler(os.path.join(os.getcwd(), 'logs/robo_scientist.log'))
+    fh.setLevel(logging.INFO)
+    ch = logging.StreamHandler()
+    ch.setLevel(logging.WARNING)
+    formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    fh.setFormatter(formatter)
+    ch.setFormatter(formatter)
+    logger.addHandler(fh)
+    logger.addHandler(ch)
+    return logger
+
+
 class RoboScientist(object):
     """
     The learning agent.
@@ -30,17 +45,7 @@ class RoboScientist(object):
         """
         self._logger = logger
         if self._logger is None:
-            self._logger = logging.getLogger(self.__class__.__name__)
-            self._logger.setLevel(logging.DEBUG)
-            fh = logging.FileHandler(os.path.join(os.getcwd(), 'logs/robo_scientist.log'))
-            fh.setLevel(logging.INFO)
-            ch = logging.StreamHandler()
-            ch.setLevel(logging.WARNING)
-            formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
-            fh.setFormatter(formatter)
-            ch.setFormatter(formatter)
-            self._logger.addHandler(fh)
-            self._logger.addHandler(ch)
+            self._logger = _create_logger(self.__class__.__name__)
         self._best_results = {}  # Dict[Tuple[Type[env_base.EnvironmentBase], Type[theory_base.TheoryBase]], theory_base.TheoryBase]
         self._logger.info(('Creating RoboScientist object with the following configurations:\n'
                            'working directories: %s\nkeep_full_history: %s') % (working_directories, keep_full_history))
@@ -66,31 +71,47 @@ class RoboScientist(object):
         self._reset_history(key)
         current_dir = os.getcwd()
         os.chdir(self._working_directories[theory_class])
+        self._logger.info(
+            'Current working directory: %s\n\tChanging to %s ...' % (current_dir,
+                                                                     self._working_directories[theory_class]))
 
         X_train = None
         generator = generator_class(new_env)
         theory = theory_class(new_env.parameters_count)
         for epoch in range(epochs):
+            self._logger.info('EPOCH %d\n' % epoch)
             X_train = generator.ask(theory, X_train)
             y_train = new_env.run_experiments(X_train)
             theory.train(X_train, y_train)
             self._update_history(key, theory, generator, new_env)
 
+        self._logger.info(
+            'Current working directory: %s\n\tChanging to %s ...' % (os.getcwd(), current_dir))
         os.chdir(current_dir)
         return deepcopy(self._best_results[key])
 
     def get_formula_for_exploration_key(self, key: ExplorationKey) -> str:
         if key in self._best_results:
             return self._best_results[key].get_formula()
+        self._logger.warning(('Formula for (%s environment, %s theory) pair does not exist. Make sure to explore the '
+                              '%s environment by using %s theory') % (key.env, key.theory, key.env, key.theory))
 
     def get_full_history(self):
         if self._keep_full_history:
             return deepcopy(self._history)
+        self._logger.warning(('keep_full_history parameter is set to False: no history tracking. To start tracking '
+                              'history make sure to set keep_full_history to True.'))
+        return None
 
     def get_history_for_exploration_key(self, key):
         if self._keep_full_history:
             if key in self._history:
                 return deepcopy(self._history[key])
+            self._logger.warning('No history is found for (%s environment, %s theory) pair.' % (key.env, key.theory))
+            return []
+        self._logger.warning(('keep_full_history parameter is set to False: no history tracking. To start tracking '
+                              'history make sure to set keep_full_history to True.'))
+        return []
 
     def _update_history(self, key, theory, generator, new_env):
         # TODO(nuwanda): add condition based on MSE here
@@ -106,12 +127,12 @@ class RoboScientist(object):
             self._best_results[key] = deepcopy(theory)
         else:
             old_mse = old_theory.calculate_test_mse(X_test, y_test)
-            old_formula = old_theory.get_formula
+            old_formula = old_theory.get_formula()
             if old_formula == formula:
                 self._logger.info(
                     ('Learnt formula is exactly the same as the previous one for (%s environment, %s theory) pair.'
                      'MSE: %f, FORMULA: %s') % (key.env, key.theory, mse, formula))
-            if old_mse < mse:
+            elif old_mse < mse:
                 self._logger.info(
                     ('The previous theory was better than the new one for (%s environment, %s theory) pair.'
                      'previous MSE: %f, new MSE: %f, keeping FORMULA: %s,'
