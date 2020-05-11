b2fa5021e4cea5a3fdeeeb2e1803b56904f0ecad
mishazybin <zybin.98@mail.ru>
2020-05-11 12:50:51 +0300

    remove verbose, add tab

--- theories/theory_nested_formulas.py
+++ theories/theory_nested_formulas.py
@@ -16,7 +16,7 @@ class TheoryNestedFormula(base.TheoryBase):
     def train(self, X, y, optimizer_for_formula=torch.optim.Adam, device=torch.device("cpu"), n_init=1,
               max_iter=100,
               lr=0.01,
-              depth=1, verbose=2, verbose_frequency=5000,
+              depth=1, verbose_frequency=5000,
               max_epochs_without_improvement=1000,
               minimal_acceptable_improvement=1e-6, max_tol=1e-5):
 
@@ -26,6 +26,10 @@ class TheoryNestedFormula(base.TheoryBase):
                 Training vector, where n_samples is the number of samples and n_features is the number of features.
             y: torch.tensor, shape (n_samples, 1)
                 Target vector relative to X.
+            optimizer_for_formula: 
+                optimizer used for solving the optimization problem
+            device:
+                device used to store the data and run the algorithm
             n_init: int
                 number of times algorithm will be run with different initial weights.
                 The final results will be the best output of n_init consecutive runs in terms of loss.
@@ -33,12 +37,8 @@ class TheoryNestedFormula(base.TheoryBase):
                 Maximum number of iterations of the algorithm for a single run.
             depth: int
                 depth of formula to learn
-            verbose: int
-                if is equal to 0, no output
-                if is equal to 1, output number of runs and losses
-                if is equal to 2, output number of runs and losses and print loss every verbose_frequency epochs
             verbose_frequency: int
-                if verbose equals 2, then print loss every verbose_frequency epochs
+                print loss every verbose_frequency epochs
             max_epochs_without_improvement: int
                 if during this number of epochs loss does not decrease more than minimal_acceptable_improvement, the learning process
                 will be finished
@@ -59,8 +59,7 @@ class TheoryNestedFormula(base.TheoryBase):
 
         for init in range(n_init):
             losses = []
-            if verbose > 0:
-                self._logger.info("  Initialization #{}".format(init + 1))
+            self._logger.info("\tInitialization #{}".format(init + 1))
             model = NestedFormula(depth, m).to(device)
 
             criterion = nn.MSELoss()
@@ -78,7 +77,7 @@ class TheoryNestedFormula(base.TheoryBase):
                 loss = criterion(output, y)
                 losses.append(loss.item())
                 loss.backward()
-                if verbose == 2 and (epoch + 1) % verbose_frequency == 0:
+                if (epoch + 1) % verbose_frequency == 0:
                     self._logger.info("\t\tEpoch {}, current loss {:.3}, current formula ".format(epoch + 1, loss.item()), end='')
                     self._logger.info(model)
                 optimizer.step()
@@ -94,8 +93,7 @@ class TheoryNestedFormula(base.TheoryBase):
             if loss < best_loss:
                 best_loss = loss
                 best_model = model
-            if verbose > 0:
-                self._logger.info("\tFinished run #{}, loss {}, best loss {}".format(init + 1, loss, best_loss))
+            self._logger.info("\tFinished run #{}, loss {}, best loss {}".format(init + 1, loss, best_loss))
             if loss < max_tol:
                 self._logger.info(f'loss is smaller than {max_tol}, terminating learning process')
                 break
